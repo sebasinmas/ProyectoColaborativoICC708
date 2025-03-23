adfe5d937ab603f399b13974d73d8f374257ab1c
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-03-23 14:48:01 -0300

    Chore: Crea una clase que lee la entrada del usuario

---
+++ src/main/java/io/github/sebastian/InputHandler.java
@@ -0,0 +1,27 @@
+package io.github.sebastian;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class InputHandler {
+    public static ArrayList<Double> getInput(){
+        ArrayList<Double> notas = new ArrayList<>();
+        Scanner sc = new Scanner("System.in");
+        System.out.println("Ingrese una por una las notas de su promedio");
+        while(true){
+            System.out.print("> ");
+            String entrada = sc.nextLine();
+            if (entrada.equalsIgnoreCase("fin") || entrada.equalsIgnoreCase("salir")){
+                break;
+            }
+            try{
+                Double nota = Double.parseDouble(entrada);
+                notas.add(nota);
+            } catch (Exception e) {
+                throw new RuntimeException("Input inválido");
+            }
+        }
+        return notas;
+    }
+}
