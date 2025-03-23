4af4e7904e6937ca55d843b0e85ed6c9a7adad12
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-03-23 14:29:15 -0300

    Rename

---
+++ src/main/java/io/github/sebastian/CalculadoraDePromedios.java
@@ -0,0 +1,9 @@
+package io.github.sebastian;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class CalculadoraDePromedios {
+    public static void main(String[] args) {
+
+    }
+}
